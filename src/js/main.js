6c90fd7cf6dcf9b9fced5190577120b9a6742aa7
Steeve <jerent.steeve@gmail.com>
2016-12-06 11:46:17 +0100

    Mise a jour

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,3 @@
-// path navbar.html
-var $path_navbar = "views/navbar.html";
-
-/**
- * after load DOM
-*/
-$(document).ready(function(){
-    // load navbar.html
-    $( "#navbar" ).load($path_navbar);
-});
 
 /**
  * after ready
@@ -17,7 +7,7 @@ setTimeout(function() {
     var $headerTitle = $('.navbar-default .navbar-brand');
     var $headerLink = $('.navbar-default .navbar-nav > li > a')
     $(document).scroll(function() {
-        $(this).scrollTop() > 25 ? $($nav).addClass("navbar-custom") :$($nav).removeClass("navbar-custom");
+        $(this).scrollTop() > 25 ? $($nav).addClass("navbar-custom") : $($nav).removeClass("navbar-custom");
         $(this).scrollTop() > 25 ? $($headerLink).addClass("inherit-color") :$($headerLink).removeClass("inherit-color");
         $(this).scrollTop() > 25 ? $($headerTitle).addClass("inherit-color") :$($headerTitle).removeClass("inherit-color");
     });
